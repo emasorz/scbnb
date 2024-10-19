d2df1ebeec7c200df99d7924fff606645671c42d
emasorz <emanuele.sorzana@outlook.it>
2024-10-19 11:00:08 +0200

    wip: test component rendering

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,13 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+import { PrimeIcons} from 'primeng/api';
+import {CalendarModule} from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ButtonModule, RouterOutlet],
+  imports: [CommonModule, ButtonModule, RouterOutlet,MenubarModule, CalendarModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular';
+  value:any;
+  items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [{
+        label: 'Home',
+       
+    },
+    {
+     label: 'About'
+    }];
+}
 }
--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-
+import { importProvidersFrom } from '@angular/core';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [importProvidersFrom([BrowserAnimationsModule]), provideRouter(routes)]
 };
